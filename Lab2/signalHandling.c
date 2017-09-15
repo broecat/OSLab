ef405fcaa04e8ea538d2737a35e7d1936ba315e3
Jose Garcia <jose.garcia12@upr.edu>
2017-09-15 10:29:04 -0400

    Lab update III:electric boogaloo

--- Lab2/signalHandling.c
+++ Lab2/signalHandling.c
@@ -5,57 +5,46 @@
 #include <stdlib.h>
 
 
-void * sigintHandle(int myint)
+void * signalHandle(int sig)
 {
-   printf("\nSignal Handled!! SIGINT\n\n");
-   exit(0);
-}
-
-void * sigtermHandle(int myint)
-{
-   printf("\nSignal Handled!! SIGTERM\n\n");
-   exit(0);
-}
-void * sigquitHandle(int myint)
-{
-   printf("\nSignal Handled!! SIGQUIT\n\n");
-   exit(0);
-}
-void * sigfpeHandle(int myint)
-{
-   printf("\nSignal Handled!! SIGFPE\n\n");
-   printf("\nNow, the program will make the SIGFPE occur programmatically.");
+    switch(sig){
+        case SIGINT:
+            printf("\nSignal Handled!! SIGINT\n\n");
+            break;
+        case SIGTERM:
+            printf("\nSignal Handled!! SIGTERM\n\n");
+            break;
+        case SIGQUIT:
+            printf("\nSignal Handled!! SIGQUIT\n\n");
+            break;
+        case SIGFPE:
+            printf("\nSignal Handled!! SIGFPE\n\n");
+            printf("\nNow, the program will make the SIGFPE occur programmatically.");
+            int a = 10/0;
+            break;
+        case SIGSEGV:
+            printf("\nSignal Handled!! SIGSEGV\n\n");
+            printf("\nNow, the program will make the SIGSEGV occur programmatically.");
+        
+            int arr[5];
+            arr[10] = 12;
+        case SIGILL:
+            printf("\nSignal Handled!! SIGILL\n\n");
+            break;
+    }
    
-   int a = 10/0;
-
-   exit(0);
-}
-void * sigsegvHandle(int myint)
-{
-   printf("\nSignal Handled!! SIGSEGV\n\n");
-   printf("\nNow, the program will make the SIGSEGV occur programmatically.");
-   
-   int arr[5];
-   arr[10] = 12;
-
-   exit(0);
-}
-void * sigillHandle(int myint)
-{
-   printf("\nSignal Handled!! SIGILL\n\n");
-
    exit(0);
 }
 
 
 int main()
 {
-   signal( SIGINT, (void *)  sigintHandle );
-   signal( SIGQUIT, (void *)  sigquitHandle );
-   signal( SIGTERM, (void *)  sigtermHandle );
-   signal( SIGFPE, (void *)  sigfpeHandle );
-   signal( SIGSEGV, (void *)  sigsegvHandle );
-   signal( SIGILL, (void *)  sigillHandle );
+   signal( SIGINT, (void (*) (int))  signalHandle );
+   signal( SIGQUIT, (void (*) (int))  signalHandle );
+   signal( SIGTERM, (void (*) (int))  signalHandle );
+   signal( SIGFPE, (void (*) (int))  signalHandle );
+   signal( SIGSEGV, (void (*) (int))  signalHandle );
+   signal( SIGILL, (void (*) (int))  signalHandle );
    signal( SIGBUS, SIG_IGN);
    //Uncomment to raise SIGILL
    //raise(SIGILL);
